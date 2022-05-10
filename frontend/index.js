d77ba1ff89ad80c436688c1475b73793970631fa
Lunarang <ecmettille@gmail.com>
2022-05-10 15:04:21 -0700

    established call to endpoint for muscle class and global variable for the base url

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    debugger;
+})
+
+// global variables
+const base_url = "http://127.0.0.1:3000"
+const muscleService = new MuscleService(base_url)
---
+++ frontend/src/muscleService.js
@@ -0,0 +1,16 @@
+// make all service calls to back end regarding muscle object
+
+class MuscleService{
+    constructor(endpoint){
+        this.endpoint = endpoint
+    }
+
+    // 1: Read/Index action
+    getMuscles(){
+        fetch('${this.endpoint}/muscles')
+        .then(resp => resp.json())
+        .then(muscles => {
+
+        })
+    }
+}
