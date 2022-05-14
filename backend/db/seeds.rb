d671f977f490cd5ce23196e8dc2ae6ecf9705768
Lunarang <ecmettille@gmail.com>
2022-05-14 11:43:54 -0700

    updated stretches to include a name

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,15 +5,15 @@ latdor = Muscle.create(name: "Latissimus Dorsi")
 exob = Muscle.create(name: "External Obliques")
 glute = Muscle.create(name: "Glute")
 
-Stretch.create(instructions: "Grasp your ankle and gently pull your heel up and back until you feel a stretch in the front of your thigh. Tighten your stomach muscles to prevent your stomach from sagging outward, and keep your knees close together.", 
+Stretch.create(name: "Quad Stretch 1", instructions: "Grasp your ankle and gently pull your heel up and back until you feel a stretch in the front of your thigh. Tighten your stomach muscles to prevent your stomach from sagging outward, and keep your knees close together.", 
 muscle: quad)
-Stretch.create(instructions: "Sit on the floor with both legs out straight. Extend your arms and reach forward by bending at the waist as far as possible while keeping your knees straight.", 
+Stretch.create(name: "Seated Toe Touch", instructions: "Sit on the floor with both legs out straight. Extend your arms and reach forward by bending at the waist as far as possible while keeping your knees straight.", 
 muscle: ham)
-Stretch.create(instructions: "Start in a half-kneeling position. As you slowly bring your right hip forward you should begin to feel a stretch in the front of your hip. Grab your back foot and squeeze your back glute to increase the stretch on your Hip Flexors.", 
+Stretch.create(name: "Half Kneeling Quad / Hip Flexor Stretch", instructions: "Start in a half-kneeling position. As you slowly bring your right hip forward you should begin to feel a stretch in the front of your hip. Grab your back foot and squeeze your back glute to increase the stretch on your Hip Flexors.", 
 muscle: psoas)
-Stretch.create(instructions: "Start on your hands and knees then slowly bring your hips back until your forehead is on the floor.  You can bring your knees wider to get a better stretch in your hips. Arch your upper back and externally rotate your shoulders to stretch your lats and chest muscles.", 
+Stretch.create(name: "Child's Pose", instructions: "Start on your hands and knees then slowly bring your hips back until your forehead is on the floor.  You can bring your knees wider to get a better stretch in your hips. Arch your upper back and externally rotate your shoulders to stretch your lats and chest muscles.", 
 muscle: latdor)
-Stretch.create(instructions: "Start by lying flat on your back then bring one leg across your body, slowly rotating your gaze and upper body in the opposite direction. The key to this stretch is using your breath to open up your rib cage and sacroiliac joint and hip area without placing too much pressure on the lower back.  If you find this stretch to be too difficult you can stack both of your knees on top of each other. Once in this position you will feel more of a stretch on the upper spine when the knees are higher, and more of a stretch on the lumbar spine when the knees are lower.", 
+Stretch.create(name: "Supine Twist", instructions: "Start by lying flat on your back then bring one leg across your body, slowly rotating your gaze and upper body in the opposite direction. The key to this stretch is using your breath to open up your rib cage and sacroiliac joint and hip area without placing too much pressure on the lower back.  If you find this stretch to be too difficult you can stack both of your knees on top of each other. Once in this position you will feel more of a stretch on the upper spine when the knees are higher, and more of a stretch on the lumbar spine when the knees are lower.", 
 muscle: exob)
-Stretch.create(instructions: "Start in a seated position slowly pull your leg to your chest and externally rotate your hip while keeping your spine straight.  You should feel this stretch in your glute.", 
+Stretch.create(name: "Seated Figure Four", instructions: "Start in a seated position slowly pull your leg to your chest and externally rotate your hip while keeping your spine straight.  You should feel this stretch in your glute.", 
 muscle: glute)
