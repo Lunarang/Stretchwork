a89790ad02f4aef63c1ec5fc0988b2f9feb09332
Lunarang <ecmettille@gmail.com>
2022-05-14 13:08:14 -0700

    added even listener that refreshes page upon clicking the title of the application

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    debugger;
-})
-
 // global variables
 const base_url = "http://127.0.0.1:3000"
-const muscleService = new MuscleService(base_url)
+const stretchService = new StretchService(base_url)
+
+// refreshes page to return to 'home'
+const h1 = document.querySelector('h1')
+h1.addEventListener('click', (e) => e.preventDefault(window.location.reload()))
+
+stretchService.getStretches()
