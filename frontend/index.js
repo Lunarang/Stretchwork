108e4edb1314e92712cc3f2bdf11a5e6c8c1e3fc
Lunarang <ecmettille@gmail.com>
2022-05-14 15:55:29 -0700

    added global variables for each service object, changed click action on title to display routine index rather than refresh page, added event listener for 'Build a Routine' button which renders appropriate form, added event listener for said form submit button

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,23 @@
 // global variables
 const base_url = "http://127.0.0.1:3000"
 const stretchService = new StretchService(base_url)
+const routineService = new RoutineService(base_url)
+const muscleService = new MuscleService(base_url)
 
-// refreshes page to return to 'home'
+// clicking on title displays routines index
 const h1 = document.querySelector('h1')
-h1.addEventListener('click', (e) => e.preventDefault(window.location.reload()))
+h1.addEventListener('click', (e) => e.preventDefault(routineService.getRoutines()))
 
-stretchService.getStretches()
+// 'Build a Routine' button action renders form
+const buildRoutine =  document.getElementById('buildRoutine')
+buildRoutine.addEventListener('click', () => Routine.renderForm()
+)
+
+// 'Submit' button action for new routine form
+Routine.routineForm.addEventListener('submit', handleSubmit)
+
+function handleSubmit(){
+    event.preventDefault()
+    RoutineService.createRoutine()
+    event.target.reset() //clears form
+}
