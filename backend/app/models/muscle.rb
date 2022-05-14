3449f8099ac32d2a0e21156568395fab511d8d43
Lunarang <ecmettille@gmail.com>
2022-05-14 09:10:50 -0700

    added validations

--- backend/app/models/muscle.rb
+++ backend/app/models/muscle.rb
@@ -1,3 +1,5 @@
 class Muscle < ApplicationRecord
     has_many :stretches
+
+    validates :name, presence: true, uniqueness: true
 end
--- backend/app/models/stretch.rb
+++ backend/app/models/stretch.rb
@@ -1,3 +1,6 @@
 class Stretch < ApplicationRecord
     belongs_to :muscle
+
+    validates :name, presence: true, uniqueness: true
+    validates :instructions, presence: true
 end
