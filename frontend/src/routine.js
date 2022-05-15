77431d88e8e42eb9a75cfd1004b2378ca2400e1d
Lunarang <ecmettille@gmail.com>
2022-05-15 14:03:46 -0700

    rendering the form now also automatically hides it from view

--- frontend/src/routine.js
+++ frontend/src/routine.js
@@ -3,13 +3,19 @@ class Routine {
     static routineContainer = document.getElementById("routine-container")
     static routineForm = document.getElementById("form-container")
 
-    constructor({name}){
+    constructor({name, muscles}){
         this.name = name;
+        // collect muscle ids
+        const newArr = muscles.map(myFunction)
+        function myFunction(muscle) {
+        return muscle.id;
+        }
+        this.muscleIds = newArr;
         // create an list element container for this instance
         this.element = document.createElement('li')
         this.element.dataset.id = this.id
         this.element.id = 'routine-${this.id}'
-        this.element.addEventListener('click', this.handleClick())
+        // this.element.addEventListener('click', this.handleClick())
         // add this instance to class array
         Routine.all.push(this)
     }
@@ -25,6 +31,15 @@ class Routine {
 
     appendDOM(){
         Routine.routineContainer.appendChild(this.routineHTML())
+    }
+
+    showRoutine(){
+        this.element.innerHTML += 
+        `
+            <h2>${this.name}</h2>
+            <button id="delete-bttn">Delete</button>
+        `
+        return this.element
     }
 
     static renderForm(){
@@ -50,11 +65,17 @@ class Routine {
             <input type="submit" id="create">
         </form>
         `
+        Routine.routineForm.style.display = 'none'
     }
 
-    handleClick = () => {
-        if (event.target.innerText === 'Delete'){
-            RoutineService.deleteRoutine(this.id)
-        }
-    }
+    // handleClick = () => {
+    //     if (event.target.innerText === 'Delete'){
+    //         RoutineService.deleteRoutine(this.id)
+    //     }
+    // }
+
+    // clicking on routine name displays routine's muscles and stretches
+    // const h2 = document.querySelector('h2')
+    // console.log(h2)
+    // h2.addEventListener('click', () => muscleService.getMuscles())
 }
