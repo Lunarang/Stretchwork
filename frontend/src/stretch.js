c37e11213558bfde944b8fe8fc939bc1422b5e1d
Lunarang <ecmettille@gmail.com>
2022-05-19 20:25:16 -0700

    cleaned up syntax and added html generating function

--- frontend/src/stretch.js
+++ frontend/src/stretch.js
@@ -1,32 +1,26 @@
 class Stretch {
-    // remember all objects within a class variable (an array)
-    // if an object gets deleted from backend, be sure to create a method to also remove it from array
     static all = []
-    static stretchContainer = document.getElementById("stretch-container")
 
-    // initialization using ES6 syntax to destruct properties and values ie: the ({})
-    constructor({name, instructions}){
+    // Initialization
+    constructor({id, name, instructions}){
+        this.id = id;
         this.name = name;
         this.instructions = instructions;
-        // create an list element container for this instance
-        this.element = document.createElement('li')
+
+        this.element = document.createElement('div')
         this.element.dataset.id = this.id
-        this.element.id = 'stretch-${this.id}'
-        // add this instance to class array
+        this.element.id = `stretch-${this.id}`
+
         Stretch.all.push(this)
     }
 
-    // by separating this from .appendDOM(), it's easier to add an 'update' feature later
+    // HTML generated
     stretchHTML(){
-        // add html to display data in this instance's list element
-        this.element.innerHTML += `
-            <h2>${this.name}</h2>
+        this.element.innerHTML += 
+        `
+            <h4>${this.name}</h4>
             <p>${this.instructions}</p>
         `
         return this.element
-    }
-
-    appendDOM(){
-        Stretch.stretchContainer.appendChild(this.stretchHTML())
     }
 }
