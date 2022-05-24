e8a244bd0a0b9ee2c64618c507a78023390305fc
Lunarang <ecmettille@gmail.com>
2022-05-23 20:30:50 -0700

    added delete button functionality, and made form dynamically generated according to the muscles/stretches available in the database

--- frontend/src/routine.js
+++ frontend/src/routine.js
@@ -6,17 +6,19 @@ class Routine{
     constructor({id, name, muscles}){
         this.id = id;
         this.name = name;
-        // collect muscle ids
+
+        // Collect muscle IDs
         const newArr = muscles.map(myFunction)
         function myFunction(muscle) {
         return muscle.id;
         }
         this.muscleIds = newArr;
-        // create an list element container for this instance
+
+        // Create div element container for this instance
         this.element = document.createElement('div')
         this.element.dataset.id = this.id
         this.element.id = `routine-${this.id}`
-        // this.element.addEventListener('click', this.handleClick())
+
         // add this instance to class array
         Routine.all.push(this)
     }
@@ -35,6 +37,15 @@ class Routine{
     appendDOM(){
         Routine.routineContainer.appendChild(this.routineHTML())
         this.renderMuscles()
+
+        // Event listener for delete button
+        this.element.addEventListener('click', this.deleteBttn)
+    }
+
+    deleteBttn = () => {
+        if (event.target.innerText === 'Delete'){
+            routineService.deleteRoutine(this.id)
+        }
     }
 
     renderMuscles(){
@@ -61,32 +72,63 @@ class Routine{
         Routine.routineForm.innerHTML += 
         `
         <form>
-            <label for="name">Name:</label><br>
+            <label for="name">Routine Name:</label><br>
             <input type="text" id="name" name="name"><br>
 
             <label for="muscles">Choose a muscle:</label><br>
             <select name="muscles" id="muscles">
-              <option value="musclename">Muscle 1</option>
-              <option value="musclename">Muscle 2</option>
-              <option value="musclename">Muscle 3</option>
+
             </select><br>
 
             <label for="stretches">Choose a stretch:</label><br>
             <select name="stretches" id="stretches">
-              <option value="stretchname">Stretch 1</option>
-              <option value="stretchname">Stretch 2</option>
-              <option value="stretchname">Stretch 3</option>
+
             </select><br>
 
+            <input type="button" id="addMuscle" value="Add Another Muscle" />
             <input type="submit" id="create">
         </form>
         `
-        Routine.routineForm.style.display = 'none'
-    }
+        // Selectors
+        const muscleSelect = document.querySelector('#muscles');
+        const stretchSelect = document.querySelector('#stretches');
+        const addMuscle = document.querySelector('#addMuscle');
 
-    // handleClick = () => {
-    //     if (event.target.innerText === 'Delete'){
-    //         RoutineService.deleteRoutine(this.id)
-    //     }
-    // }
+        // Add muscles to dropdown menu
+        const muscles = Muscle.all;
+
+        for (const muscle of muscles){
+            const html = `<option value="${muscle.name}">${muscle.name}</option>`
+            muscleSelect.insertAdjacentHTML('beforeend', html)
+        }
+
+        // Action when a muscle is selected
+        muscleSelect.onchange = function() {
+            // Clear stretch options
+            stretchSelect.innerHTML = "";
+
+            // Find muscle object and it's stretches
+            const selected = muscles.find(muscle => muscle.name === this.value)
+            const stretches = Stretch.all.filter(findStretch, selected.stretchIds);
+
+            function findStretch (value){
+                return this.includes(value.id);
+            }
+
+            // Add associated stretches to drop down menu
+            for (const stretch of stretches){
+                const stretchHtml = `<option value="${stretch.name}">${stretch.name}</option>`
+                stretchSelect.insertAdjacentHTML('beforeend', stretchHtml)
+            }
+        }
+
+        // Click on Add Muscle button
+        // addMuscle.addEventListener('click', makeInputs);
+
+        // function makeInputs(){
+
+        // }
+
+        Routine.routineForm.style.display = 'none';
+    }
 }
