6569adf3da3c195b5a9f7be614b9503f36380d6c
Lunarang <ecmettille@gmail.com>
2022-05-23 20:31:17 -0700

    added post and delete fetch requests

--- frontend/src/routineService.js
+++ frontend/src/routineService.js
@@ -14,40 +14,55 @@ class RoutineService{
                 const r = new Routine(routine)
                 r.appendDOM()
             }
+            Routine.renderForm();
         })
     }
 
-    // // 2: Create action
-    // createRoutine(){
-    //     const routine = {
-    //         name: document.getElementById('name').value,
-    //     }
+    // 2: Create action
+    createRoutine(event){
+        const data = {
+            name: event.target.dataset.name,
 
-    //     const configObj = {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //         body: JSON.stringify(contact)
-    //     }
+        }
         
-    //     fetch(`${this.endpoint}/routines`)
-    //     .then(resp => resp.json())
-    //     .then(routine => {
-    //         const r = new Routine(routine)
-    //         r.appendDOM()
-    //     })
-    // }
+        fetch(`${this.endpoint}/routines`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+        .then(routine => {
+            const r = new Routine(routine)
+            r.appendDOM()
+        })
+    }
 
-    // // 3: Delete action
-    // deleteRoutine(id){
-    //     fetch(`${this.endpoint}/routines/${id}`, {
-    //         method: 'DELETE',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     })
-    //     .then(resp => resp.json())
-    //     .then(json => alert(json.message))
-    // }
+    // 3: Delete action
+    deleteRoutine(id){
+        // Delete from database
+        fetch(`${this.endpoint}/routines/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+        .then(resp => resp.json())
+        .then(json => alert(json.message))
+
+        // Delete from JS memory
+        const routines = Routine.all;
+        const deleted = routines.find(routine => routine.id === id);
+        const index = routines.indexOf(deleted);
+
+        if (index > -1) {
+            routines.splice(index, 1);
+        }
+
+        // Refresh page to update DOM
+        window.location.reload();
+    }
 }
